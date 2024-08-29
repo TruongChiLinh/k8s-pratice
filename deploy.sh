d838285e74f1b02cc12794e8c06629cf471680d0
JayTruong <Jaytruong@5427gmail.com>
2024-08-29 22:34:12 +0700

    commit dau tien

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+kubectl apply -f deployment.yml
+kubectl apply -f daemonset.yml
+kubectl apply -f replicaset.yml
+kubectl apply -f service.yml
+kubectl apply -f statefullset.yml
+kubectl apply -f jobs.yml
+kubectl apply -f cronjob.yml
---
+++ shutdown.sh
@@ -0,0 +1,7 @@
+kubectl delete -f deployment.yml
+kubectl delete -f daemonset.yml
+kubectl delete -f replicaset.yml
+kubectl delete -f service.yml
+kubectl delete -f statefulset.yml
+kubectl delete -f jobs.yml
+kubectl delete -f cronjob.yml
